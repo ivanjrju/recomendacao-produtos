d9dd3ab52cedf2ed6924c1346f944b47180f9970
ivan <juniorivandias@gmail.com>
2024-09-14 10:45:27 -0300

    host

--- api.py
+++ api.py
@@ -26,4 +26,4 @@ def recomendar_produtos(cliente):
     return jsonify({'cliente': cliente, 'produtos': lista, 'oferta': oferta})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
